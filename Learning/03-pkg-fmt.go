194c343259ec6ada4bfe2f1227e913b547d04463
yazbek1989 <yazbek1989@gmail.com>
2022-11-11 08:10:22 -0600

    modified:   Learning/03-pkg-fmt.go

--- Learning/03-pkg-fmt.go
+++ Learning/03-pkg-fmt.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func Format() {
-	fmt.Println("test")
+	hola := "Hola"
+	mundo := "Mundo"
+	fmt.Println(hola, ", ", mundo)
+
+	nombre := "Iván"
+	edad := 33
+	fmt.Printf("Hola, %s y su edad es %d \n", nombre, edad)
+	fmt.Printf("Hola, %v y su edad es %v \n", nombre, edad)
 }
