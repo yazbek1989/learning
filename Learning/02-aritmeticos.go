0d79f5e5c7c5eeb7ca15f3b2ec268c73816ae4cf
yazbek1989 <yazbek1989@gmail.com>
2022-11-11 07:42:26 -0600

    modified:   Learning/02-aritmeticos.go 	new file:   Learning/03-pkg-fmt.go 	modified:   Learning/go.mod 	modified:   Learning/main.go

--- Learning/02-aritmeticos.go
+++ Learning/02-aritmeticos.go
@@ -3,8 +3,34 @@ package main
 import "fmt"
 
 func Aritmeticos() {
-	a := 20
+	a := 21
 	b := 10
+
+	fmt.Println("el valor de a es:", a)
+	fmt.Println("el valor de b es:", b)
+
+	//suma
 	result := a + b
-	fmt.Println(result)
+	fmt.Println("suma:", result)
+
+	//resta
+	result = a - b
+	fmt.Println("resta:", result)
+
+	//multiplicacion
+	result = a * b
+	fmt.Println("multiplicacion:", result)
+
+	//division
+	result = a / b
+	fmt.Println("division:", result)
+
+	//division decimal
+	var div float64 = 3.0 / 2.0
+	fmt.Println("division:", div)
+
+	//modulo
+	result = a % b
+	fmt.Println("modulo:", result)
+
 }
---
+++ Learning/03-pkg-fmt.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Format() {
+	fmt.Println("test")
+}
