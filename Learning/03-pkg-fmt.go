379326154f309e652ee145485364973182481a66
yazbek1989 <yazbek1989@gmail.com>
2022-11-14 07:11:00 -0600

    modified:   Learning/03-pkg-fmt.go

--- Learning/03-pkg-fmt.go
+++ Learning/03-pkg-fmt.go
@@ -9,6 +9,14 @@ func Format() {
 
 	nombre := "Iván"
 	edad := 33
+	//%T es para ver que tipo de dato es
+	fmt.Printf("nombre: %T \n", nombre)
+	//%s es para string y %d es para entero
 	fmt.Printf("Hola, %s y su edad es %d \n", nombre, edad)
+	//en printf %v es para cualquier tipo de variable
 	fmt.Printf("Hola, %v y su edad es %v \n", nombre, edad)
+	//sprintf es para guardar un mensaje en una variable
+	mensaje := fmt.Sprintf("Hola, %v y su edad es %v", nombre, edad)
+	fmt.Println(mensaje)
+
 }
