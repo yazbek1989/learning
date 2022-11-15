828819af21a22ceb6a5e1a2c6823d0bc87ac6f24
yazbek1989 <yazbek1989@gmail.com>
2022-11-15 08:16:02 -0600

    new file:   Learning/01-04 practica.go 	modified:   Learning/04-funciones.go 	modified:   Learning/main.go

--- Learning/04-funciones.go
+++ Learning/04-funciones.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// funcion para imprimirr nombre y texto segun recibido
 func Funciones(nombre string) {
 	fmt.Println("Hola, ", nombre)
+}
+
+//funcion para devolver valor entero e imprimirlo en el main
+func Sumar(a, b int) int {
+	total := a + b
+	return total
 }
