2fb163c621a9e8549d2567489c3e73c9c76a8530
yazbek1989 <yazbek1989@gmail.com>
2022-12-12 05:08:32 -0600

    modified:   Learning/07-slicesfuncmake.go

--- Learning/07-slicesfuncmake.go
+++ Learning/07-slicesfuncmake.go
@@ -1,5 +1,14 @@
 package main
 
-func make() {
-	numeros
+import "fmt"
+
+func funcmake() {
+	numeros := make([]int, 0, 3)
+	// numeros[0] = 100
+	// numeros[1] = 200
+	// numeros[2] = 300
+
+	numeros = append(numeros, 400)
+
+	fmt.Println(numeros, len(numeros), cap(numeros))
 }
